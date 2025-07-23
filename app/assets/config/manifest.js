9466a00685614e712885849b2bdfd576e25d11c3
sebastianpjj <sebastian.pj84@googlemail.com>
2025-07-23 23:31:00 +0200

    Fix asset manifest - remove invalid JavaScript linking and use proper paths

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,4 @@
 //= link_tree ../images
 //= link application.css
 //= link active_admin.scss
-//= link active_admin.js
-//= link application.js
-//= link react_components.js
-//= link_tree ../javascript .js
+//= link_tree ../javascripts .js

c37ef5dcb27a49b5f60de05206e1006a531423f1
sebastianpjj <sebastian.pj84@googlemail.com>
2025-07-24 00:16:44 +0200

    Fix React component loading - remove from Sprockets manifest, use importmaps only

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,3 @@
 //= link_tree ../images
 //= link application.css
 //= link active_admin.scss
-//= link controllers/application.js
-//= link controllers/index.js
-//= link components/EventDetail.js
-//= link components/EventsList.js

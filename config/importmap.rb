cda4a55f67402805da7fc61f4107ca325deed941
sebastianpjj <sebastian.pj84@googlemail.com>
2025-07-30 20:00:34 +0200

    Revert to standard importmap configuration compatible with current version

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", integrity: true
+pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers", integrity: true
+pin_all_from "app/javascript/controllers", under: "controllers"
 
 # React dependencies - using CDN for reliable delivery
 pin "react", to: "https://ga.jspm.io/npm:react@18.2.0/index.js"
@@ -12,6 +12,6 @@ pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18.2.0/index.js"
 pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.23.0/index.js"
 pin "react-dom/client", to: "https://ga.jspm.io/npm:react-dom@18.2.0/client.js"
 
-# Local React components and initializer with automatic cache busting
-pin "react_components", to: "react_components.js", integrity: true
-pin_all_from "app/javascript/components", under: "components", integrity: true
+# Local React components and initializer - Rails asset pipeline handles cache busting
+pin "react_components", to: "react_components.js"
+pin_all_from "app/javascript/components", under: "components"

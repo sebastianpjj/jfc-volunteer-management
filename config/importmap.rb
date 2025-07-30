366f2c92265aadc22202ba8db37645adbd722a1f
sebastianpjj <sebastian.pj84@googlemail.com>
2025-07-30 22:57:15 +0200

    Temporarily disable application pin to fix 404 error

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
+# Temporarily comment out application pin to fix 404
+# pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"

cd2beb27a83a91769bdc42ec3cf903d96faa6623
sebastianpjj <sebastian.pj84@googlemail.com>
2025-07-31 01:11:55 +0200

    Fix importmap to use direct paths instead of digest hashes for React components

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/components", under: "components"
 
 # React dependencies
 pin "react", to: "https://ga.jspm.io/npm:react@18.2.0/index.js"
@@ -13,4 +12,7 @@ pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18.2.0/index.js"
 pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.23.0/index.js"
 pin "react-dom/client", to: "https://ga.jspm.io/npm:react-dom@18.2.0/client.js"
 pin "react_components", to: "react_components.js"
-pin_all_from "app/javascript/components", under: "components"
+
+# Pin React components individually without digest to avoid caching issues
+pin "components/EventDetail", to: "components/EventDetail.js", preload: false
+pin "components/EventsList", to: "components/EventsList.js", preload: false
